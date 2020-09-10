421980b1733412750756f5cb9e70112f39cddd7c
sanjeevyg <yogi.sjv@gmail.com>
2020-09-10 15:45:12 -0600

    css and js files updated

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-
-const apiKey = "eccae21236f50571310ed5eb92155abe"
-
 let baseImageURL = "https://image.tmdb.org/t/p/w185";
 
-
 fetch("http://localhost:3000/movies")
-
     .then(response => response.json())
     .then(movies => handle(movies))
 
@@ -19,48 +14,42 @@ const movieList = document.querySelector("#main-page")
 function render(movie) {   
     const title = document.createElement("div")
     title.innerText = movie.title
-    console.log(movie.title)
     title.className = "overview"
-
-    // const rating = document.createElement("h3")
-    // rating.innerText = movie.rating
-    // rating.className = "rating"
-
+    
     const space1 = document.createElement("br")
-    // const space2 = document.createElement("br")
-
-    // const releaseDate = document.createElement("div")
-    // releaseDate.innerText = `Release Date: ${movie.year}`
-    // releaseDate.className = "overview"
-
+    const space2 = document.createElement("br")
+    
     const image = document.createElement("div")
     image.innerHTML = `<a href="movie.html?id=${movie.id}"><img src="${baseImageURL}${movie.image}"></a>`
     image.className = "image"
-
+    
     image.appendChild(title)
-
-    // const plot = document.createElement("div")
-    // plot.innerText = movie.plot
-    // plot.className = "overview"
-
-    // console.log(image)
-    movieList.append(image, space1 )
+    movieList.append(image, space1)
 }
+
 
 fetch('http://localhost:3000/users')
     .then(response => response.json())
     .then(users => createUserOptions(users))
 
-    function createUserOptions(users) {
-        const userField = document.querySelector('#username-field')
+function createUserOptions(users) {
+    const userField = document.querySelector('#username-field')
+    
+    users.map(user => {
+        const userOption = document.createElement('option')
+        
+        userOption.value = user.id
+        userOption.textContent = user.name
+        
+        userField.appendChild(userOption)
+    })
+}
 
-        users.map(user => {
-            const userOption = document.createElement('option')
 
-            userOption.value = user.id
-            userOption.textContent = user.name
-
-            userField.appendChild(userOption)
-        })
-    }
-
+// function seeAllMovies {
+    //     const h3 = document.createElement("h3")
+    //     h3.innerHTML = <a href="movies.html">See All Movies</a>
+    
+    // }
+    
+// const apiKey = "eccae21236f50571310ed5eb92155abe"
--- movies.js
+++ movies.js
@@ -1,15 +1,30 @@
-console.log("All Movies")
+let baseImageURL = "https://image.tmdb.org/t/p/w185";
 
 fetch('http://localhost:3000/movies')
     .then(response => response.json())
-    .then(movies => {
-        const movieList = document.querySelector('#movies')
+    .then(movies => render(movies))
         
-        movies.forEach(movies => {
-            const movieElement = document.createElement('div')
+        
+const movieList = document.querySelector(".wrapper")
 
-            movieElement.textContent = movies.title
+function render(movies) {
+    movies.forEach(movie => {
+        const movieElement = document.createElement('div')
+        movieElement.innerHTML = `<a href="movie.html?id=${movie.id}"><img src="${baseImageURL}${movie.image}"></a>`
+        
+        const title = document.createElement('div')
+        title.innerText = movie.title
+        title.className = "text"
 
-            movieList.appendChild(movieElement)
-        })
+        movieElement.appendChild(title)
+        movieList.appendChild(movieElement)
     })
+}
+
+
+
+// function four() {
+//     for (i = 0; i < movieList.length; i++) {
+//       movieList[i].style.flex = "25%";
+//     }
+// }
